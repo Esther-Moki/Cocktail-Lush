f43f8862ad76c101126a0bd2c81dc51c8e978db8
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-16 10:11:03 +0300

    added an intent to link to the cocktail activity

--- app/src/main/java/com/moringaschool/cocktaillush/MainActivity.java
+++ app/src/main/java/com/moringaschool/cocktaillush/MainActivity.java
@@ -4,25 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindCocktailButton;
+    private EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
         mFindCocktailButton = (Button)findViewById(R.id.findCocktailButton);
         mFindCocktailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mNameEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, CocktailActivity.class);
                 startActivity(intent);
-                 // Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+               //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
         });
     }
