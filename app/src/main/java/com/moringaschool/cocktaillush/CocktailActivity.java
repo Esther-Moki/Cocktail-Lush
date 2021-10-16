770c5f266879bc394649c0ead3ceaad92f507bd4
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-16 10:18:56 +0300

    changed the cocktail activity to a relative layout

--- app/src/main/java/com/moringaschool/cocktaillush/CocktailActivity.java
+++ app/src/main/java/com/moringaschool/cocktaillush/CocktailActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.cocktaillush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CocktailActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class CocktailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
     }
 }
--- app/src/main/java/com/moringaschool/cocktaillush/MainActivity.java
+++ app/src/main/java/com/moringaschool/cocktaillush/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
+  //  public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindCocktailButton;
     private EditText mNameEditText;
 
@@ -22,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         mNameEditText = (EditText) findViewById(R.id.nameEditText);
         mFindCocktailButton = (Button)findViewById(R.id.findCocktailButton);
+
         mFindCocktailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String location = mNameEditText.getText().toString();
-                Log.d(TAG, location);
+                String name = mNameEditText.getText().toString();
+               // Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, CocktailActivity.class);
+                intent.putExtra("name", name);
                 startActivity(intent);
                //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
