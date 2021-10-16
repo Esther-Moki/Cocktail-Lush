c2646631045925e8b5d373a8041eb53e639f5a80
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-16 09:35:01 +0300

    changed the button background color

--- app/src/main/java/com/moringaschool/cocktaillush/MainActivity.java
+++ app/src/main/java/com/moringaschool/cocktaillush/MainActivity.java
@@ -3,12 +3,24 @@ package com.moringaschool.cocktaillush;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindCocktailButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindCocktailButton = (Button)findViewById(R.id.findCocktailButton);
+        mFindCocktailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
