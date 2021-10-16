744408a3165e69d12c1ebd4c064222509eacfc05
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-16 10:32:39 +0300

    created a main activity test

--- app/src/main/java/com/moringaschool/cocktaillush/CocktailActivity.java
+++ app/src/main/java/com/moringaschool/cocktaillush/CocktailActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CocktailActivity extends AppCompatActivity {
-
+    private TextView mNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail);
+        mNameTextView = (TextView) findViewById(R.id.nameTextView);
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
+        mNameTextView.setText("Here are all the " + name + " cocktails" );
 
     }
 }
